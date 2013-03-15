c35187f1010289152f4e03b9f6b2b192581afdc2
tokuhirom <tokuhirom@gmail.com>
2013-03-15 21:32:54 +0900

    more lang.

--- lib/LiBot/Plugin/LLEval.pm
+++ lib/LiBot/Plugin/LLEval.pm
@@ -12,9 +12,9 @@ use Mouse;
 no Mouse;
 
 sub lleval {
-    my $src = shift;
+    my ($src, $lang) = @_;
     my $ua = Furl->new(agent => 'lleval2lingr', timeout => 5);
-    my $res = $ua->get('http://api.dan.co.jp/lleval.cgi?l=pl&s=' . uri_escape_utf8($src));
+    my $res = $ua->get(sprintf('http://api.dan.co.jp/lleval.cgi?l=%s&s=%s', $lang, uri_escape_utf8($src)));
     $res->is_success or die $res->status_line;
     print $res->content, "\n";
     return decode_json($res->content);
@@ -27,10 +27,16 @@ sub init {
         qr/^!\s*(.*)/ => sub {
             my ( $cb, $event, $code ) = @_;
 
-            unless ( $code =~ m{^(print|say)} ) {
-                $code = "print sub { ${code} }->()";
+            my $lang = 'pl';
+            if ($code =~ /^!\s*([a-z]+)\s*(.+)/) {
+                $lang = $1;
+                $code = $2;
+            } else {
+                unless ( $code =~ m{^(print|say)} ) {
+                    $code = "print sub { ${code} }->()";
+                }
             }
-            my $res = lleval($code);
+            my $res = lleval($code, $lang);
             if ( defined $res->{error} ) {
                 $cb->( shorten_scalar( $res->{error}, 80 ) );
             }
