fe4fee8141f4b2c14ef8b3a9c1d276e5a32ee27a
tokuhirom <tokuhirom@gmail.com>
2013-03-15 15:11:08 +0900

    forward mention

--- cpanfile
+++ cpanfile
@@ -1,3 +1,4 @@
 requires 'Pod::PerldocJp';
 requires 'Text::Shorten';
 requires 'JSON';
+requires 'ANyEvent::IRC';
--- perl-bot.pl
+++ perl-bot.pl
@@ -7,17 +7,189 @@ use WebService::Lingr::Bot;
 use Furl;
 use URI::Escape qw(uri_escape_utf8);
 use JSON qw(decode_json);
-use Encode qw(decode_utf8);
+use Encode qw(decode_utf8 encode_utf8);
 use Text::Shorten qw(shorten_scalar);
 use Getopt::Long;
 use AE;
+use AnyEvent::IRC::Client;
+use GDBM_File;
 
 my $host = '127.0.0.1';
 my $port = 6000;
+my $karma_dict_path = 'karma.db';
 GetOptions(
     'host=s' => \$host,
     'port=s' => \$port,
+    'karma-dict=s' => \$karma_dict_path,
 ) or die;
+
+my $irc = setup_irc();
+my $bot = setup_bot();
+my $server = $bot->run( host => $host, port => $port );
+print "http://$host:$port/\n";
+
+AE::cv->recv;
+
+sub setup_bot {
+    my $bot = WebService::Lingr::Bot->new();
+    $bot->register(
+        qr/^!\s*(.*)/ => sub {
+            my ( $cb, $event, $code ) = @_;
+
+            unless ( $code =~ m{^(print|say)} ) {
+                $code = "print sub { ${code} }->()";
+            }
+            my $res = lleval($code);
+            if ( defined $res->{error} ) {
+                $cb->( shorten_scalar( $res->{error}, 80 ) );
+            }
+            else {
+                $cb->( shorten_scalar( $res->{stdout} . $res->{stderr}, 80 ) );
+            }
+        }
+    );
+    $bot->register(
+        qr/^perldoc\s+(.*)/ => sub {
+            my ( $cb, $event, $arg ) = @_;
+
+            pipe( my $rh, my $wh );
+
+            my $pid = fork();
+            $pid // do {
+                close $rh;
+                close $wh;
+                die $!;
+            };
+
+            if ($pid) {
+
+                # parent
+                close $wh;
+
+                my $ret = '';
+                my $sweep;
+                my $timer = AE::timer(
+                    10, 0,
+                    sub {
+                        kill 9, $pid;
+                    }
+                );
+                my $child;
+                $child = AE::child(
+                    $pid,
+                    sub {
+                        undef $timer;
+                        $ret =~ s/NAME\n//;
+                        $ret =~ s/\nDESCRIPTION\n/\n/;
+                        $ret = shorten_scalar( decode_utf8($ret), 120 );
+                        if ( $arg =~ /\A[\$\@\%]/ ) {
+                            $ret .= "\n\nhttp://perldoc.jp/perlvar";
+                        }
+                        elsif ( $arg =~ /\A-[a-z]\s+(.+)/ ) {
+                            $ret .= "\n\nhttp://perldoc.jp/$1";
+                        }
+                        else {
+                            $ret .= "\n\nhttp://perldoc.jp/$arg";
+                        }
+                        $cb->($ret);
+                        undef $sweep;
+                        undef $child;
+                    }
+                );
+                $sweep = AE::io(
+                    $rh, 0,
+                    sub {
+                        $ret .= scalar(<$rh>);
+                    }
+                );
+            }
+            else {
+                # child
+                close $rh;
+
+                open STDERR, '>&', $wh
+                  or die "failed to redirect STDERR to logfile";
+                open STDOUT, '>&', $wh
+                  or die "failed to redirect STDOUT to logfile";
+
+                eval {
+                    require Pod::PerldocJp;
+                    local @ARGV = split /\s+/, $arg;
+                    if ( @ARGV == 1 && $ARGV[0] =~ /^[\$\@\%]/ ) {
+                        unshift @ARGV, '-v';
+                    }
+                    unshift @ARGV, '-J';
+                    @ARGV = map { encode_utf8($_) } @ARGV;
+                    Pod::PerldocJp->run();
+                };
+                warn $@ if $@;
+
+                exit 0;
+            }
+        }
+    );
+    tie my %karma_dict, 'GDBM_File', $karma_dict_path, &GDBM_WRCREAT, 0640;
+    $bot->register(
+        qr/(\w+)(\+\+|--)/ => sub {
+            my ($cb, $event, $name, $op) = @_;
+            $karma_dict{$name} += 1 if $op eq '++';
+            $karma_dict{$name} -= 1 if $op eq '--';
+            $cb->("$name: $karma_dict{$name}");
+        }
+    );
+    $bot->register(
+        qr/@[a-zA-Z_-]+/ => sub {
+            my ( $cb, $event, $arg ) = @_;
+            print "Send mention\n";
+            my $nickname = $event->{message}->{nickname};
+            substr($nickname, 1, 1) = '*'; # do not highlight me.
+            my $msg = sprintf("(%s) %s", $nickname, $event->{message}->{text});
+            $irc->send_chan('#hiratara', 'PRIVMSG', '#hiratara', encode_utf8($msg));
+            $cb->('');
+        }
+    );
+    $bot;
+}
+
+sub setup_irc {
+    my $irc = AnyEvent::IRC::Client->new;
+    $irc->reg_cb (registered => sub {
+        my $con = shift;
+        print "I'm in!\n";
+        $con->enable_ping(10);
+        $con->send_srv('JOIN', '#hiratara');
+    });
+    $irc->reg_cb (join => sub {
+        print "Join\n";
+    });
+    if ($ENV{DEBUG}) {
+        $irc->reg_cb(
+            debug_recv => sub {
+                my $ircmsg = shift;
+                print "[DEBUG-RECV] $ircmsg\n";
+            },
+            debug_send => sub {
+                my $ircmsg = shift;
+                print "[DEBUG-SEND] $ircmsg\n";
+            },
+        );
+    }
+    $irc->reg_cb (disconnect => sub { print "I'm out!\n"; });
+    $irc->reg_cb (connect => sub {
+        my ($con, $err) = @_;
+        if (defined $err) {
+            warn "IRC connect error: $err\n";
+            exit 0;
+        }
+        print "Connected.\n";
+    });
+    $irc->reg_cb (error => sub {
+        my ($code, $message, $ircmsg) = @_;
+        print "Error: $code, $message, $ircmsg\n";
+    });
+    $irc->connect('chat.freenode.net', 6667, {nick => 'perl_lingr_bot'});
+    return $irc;
+}
 
 sub lleval {
     my $src = shift;
@@ -27,85 +199,3 @@ sub lleval {
     print $res->content, "\n";
     return decode_json($res->content);
 }
-
-my $bot = WebService::Lingr::Bot->new();
-$bot->register(qr/^!\s*(.*)/ => sub {
-    my ($cb, $event, $code) = @_;
-
-    unless ($code =~ m{^(print|say)}) {
-        $code = "print sub { ${code} }->()";
-    }
-    my $res = lleval($code);
-    if (defined $res->{error}) {
-        $cb->(shorten_scalar($res->{error}, 80));
-    } else {
-        $cb->(shorten_scalar($res->{stdout} . $res->{stderr}, 80));
-    }
-});
-$bot->register(qr/^perldoc\s+(.*)/ => sub {
-    my ($cb, $event, $arg) = @_;
-
-    pipe(my $rh, my $wh);
-
-    my $pid = fork();
-    $pid // do {
-        close $rh;
-        close $wh;
-        die $!;
-    };
-
-    if ($pid) {
-        # parent
-        close $wh;
-
-        my $ret = '';
-        my $sweep;
-        my $timer = AE::timer(10, 0, sub {
-            kill 9, $pid;
-        });
-        my $child; $child = AE::child($pid, sub {
-            undef $timer;
-            $ret =~ s/NAME\n//;
-            $ret =~ s/\nDESCRIPTION\n/\n/;
-            $ret = shorten_scalar(decode_utf8($ret), 120);
-            if ($arg =~ /\A[\$\@\%]/) {
-                $ret .= "\n\nhttp://perldoc.jp/perlvar";
-            } elsif ($arg =~ /\A-[a-z]\s+(.+)/) {
-                $ret .= "\n\nhttp://perldoc.jp/$1";
-            } else {
-                $ret .= "\n\nhttp://perldoc.jp/$arg";
-            }
-            $cb->($ret);
-            undef $sweep;
-            undef $child;
-        });
-        $sweep = AE::io($rh, 0, sub {
-            $ret .= scalar(<$rh>);
-        });
-    } else {
-        # child
-        close $rh;
-
-        open STDERR, '>&', $wh
-          or die "failed to redirect STDERR to logfile";
-        open STDOUT, '>&', $wh
-          or die "failed to redirect STDOUT to logfile";
-
-        eval {
-            require Pod::PerldocJp;
-            local @ARGV = split /\s+/, $arg;
-            if (@ARGV == 1 && $ARGV[0] =~ /^[\$\@\%]/) {
-                unshift @ARGV, '-v';
-            }
-            unshift @ARGV, '-J';
-            Pod::PerldocJp->run();
-        };
-        warn $@ if $@;
-
-        exit 0;
-    }
-});
-my $server = $bot->run(host => $host, port => $port);
-print "http://$host:$port/\n";
-AE::cv->recv;
-
